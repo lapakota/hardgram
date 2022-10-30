f8c14c7d44cb08b75215af44cd1ceba026190e2f
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2022-10-30 19:43:14 +0500

    add annotations to main

--- server/src/main/java/hardsign/server/ServerApplication.java
+++ server/src/main/java/hardsign/server/ServerApplication.java
@@ -2,12 +2,21 @@ package hardsign.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
 public class ServerApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		try
+		{
+			SpringApplication.run(ServerApplication.class, args);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
 	}
-
 }
