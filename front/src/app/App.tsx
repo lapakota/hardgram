4396e5ba81d47c7dd6578651f7f2aed8dfc5687a
lapakota <stalkerzone955@gmail.com>
2022-10-30 20:46:38 +0500

    install libs and add linter config

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className={styles.app}>
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
