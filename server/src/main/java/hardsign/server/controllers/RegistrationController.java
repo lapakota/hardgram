61ff1770fc9dc291203c0dbbf7428834d88355d2
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2022-11-04 00:03:12 +0500

    add @CrossOrigin to RegistrationController

--- server/src/main/java/hardsign/server/controllers/RegistrationController.java
+++ server/src/main/java/hardsign/server/controllers/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.inject.Inject;
 
 @RestController
+@CrossOrigin
 public class RegistrationController {
     private final UserService userService;
 
